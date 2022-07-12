cf6e0183e24f1dab545e9276294f4de985e3cca0
Samantha-Ruth <sam.haberman@gmail.com>
2022-07-12 07:47:56 -0600

    changed connection file

--- config/connection.js
+++ config/connection.js
@@ -1,30 +1,52 @@
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
 
-require('dotenv').config();
+// require('dotenv').config();
 
-let sequelize;
+// let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-  const sequelize = process.env.JAWSDB_URL
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+//   const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       dialectOptions: {
+//         decimalNumbers: true,
+//       },
+//     });
+// } else {
+//   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     dialectOptions: {
+//               decimalNumbers: true,
+//             },
+//     port: 3306
+//   });
+// }
+
+require("dotenv").config();
+
+const Sequelize = require("sequelize");
+
+const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
-} else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: 'localhost',
-    dialect: 'mysql',
-    dialectOptions: {
-              decimalNumbers: true,
-            },
-    port: 3306
-  });
-}
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: "localhost",
+        dialect: "mysql",
+        dialectOptions: {
+          decimalNumbers: true,
+        },
+      }
+    );
+
+module.exports = sequelize;
+
 // module.exports = sequelize;
 
 // const sequelize = process.env.JAWSDB_URL
